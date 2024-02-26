e8c103a08935fdb39ad37841d8d36ae49273a9ee
fredriknystrom <fredriknystroms@gmail.com>
2024-02-26 15:14:30 +0100

    favorite blog test 4.5

--- tests/totalLikes.test.js
+++ tests/totalLikes.test.js
@@ -55,4 +55,59 @@ describe('total likes', () => {
         const result = listHelper.totalLikes(manyBlogs)
         assert.strictEqual(result, 46)
     })
-  })
+})
+
+describe('Favorite blog tests', () => {
+    const listWithOneBlog = [
+        {
+          _id: '5a422aa71b54a676234d17f8',
+          title: 'Go To Statement Considered Harmful',
+          author: 'Edsger W. Dijkstra',
+          url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+          likes: 5,
+          __v: 0
+        }
+      ]
+  
+      const manyBlogs = [
+          {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+          },
+          {
+              _id: '5a422aa71b543276234d17f8',
+              title: 'Go To Statement Considered Harmful',
+              author: 'Edsger W. Dijkstra',
+              url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+              likes: 1,
+              __v: 0
+            },
+            {
+              _id: 'b2422aa71b54a676234d17f8',
+              title: 'Go To Statement Considered Harmful',
+              author: 'Edsger W. Dijkstra',
+              url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+              likes: 40,
+              __v: 0
+            },
+        ]
+
+        test('when list is empty', () => {
+            const result = listHelper.favoriteBlog([])
+            assert.strictEqual(result, null)
+        })
+    
+        test('when list has only one blog, return that blog', () => {
+            const result = listHelper.favoriteBlog(listWithOneBlog)
+            assert.deepStrictEqual(result, listWithOneBlog[0])
+        })
+    
+        test('when list has many blogs, return blog with most likes', () => {
+            const result = listHelper.favoriteBlog(manyBlogs)
+            assert.deepStrictEqual(result, manyBlogs[2])
+        })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,7 +5,18 @@ const totalLikes = (blogs) => {
 
     return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
   }
+
+  const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+      return null
+    }
+  
+    return blogs.reduce((max, blog) => {
+      return blog.likes > max.likes ? blog : max
+    }, blogs[0])
+  };
   
   module.exports = {
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
