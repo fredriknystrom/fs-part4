096a7aa81402c936f39efeacb65a2b1e8b8cb668
fredriknystrom <fredriknystroms@gmail.com>
2024-02-26 15:04:41 +0100

    totalLikes test 4.4

--- utils/for_testing.js
+++ utils/for_testing.js
@@ -5,17 +5,17 @@ const reverse = (string) => {
       .join('')
   }
   
-  const average = array => {
-    const reducer = (sum, item) => {
-      return sum + item
-    }
-  
-    return array.length === 0
-      ? 0
-      : array.reduce(reducer, 0) / array.length
+const average = array => {
+  const reducer = (sum, item) => {
+    return sum + item
   }
-  
-  module.exports = {
-    reverse,
-    average,
-  }
+
+  return array.length === 0
+    ? 0
+    : array.reduce(reducer, 0) / array.length
+}
+
+module.exports = {
+  reverse,
+  average,
+}
