a719549b9b9df9e05d8e136887aae68f07c5a7a4
fredriknystrom <fredriknystroms@gmail.com>
2024-02-26 20:20:58 +0100

    test 4.9

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -4,14 +4,25 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const { oneBlog, multipleBlogs } = require('./test_helper')
+const blog = require('../models/blog')
 
 const api = supertest(app)
 
-test.only('returns all notes ', async () => {
+test.only('returns all notes', async () => {
     const n_expected_notes = 3
     const response = await api.get('/api/blogs')
     assert(response.body.length, n_expected_notes)
 })
+
+test.only('blog post unique identifier is named id', async () => {
+  const response = await api.get('/api/blogs')
+  if (response.body.length > 0) {
+    const blogPost = response.body[0];
+    assert(blogPost.hasOwnProperty('id'))
+  } else {
+    throw new Error('No blog posts found');
+  }
+})  
 
 after(async () => {
   await mongoose.connection.close()
