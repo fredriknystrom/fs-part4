820d7803080c27de349dd7a3d47832193371188d
fredriknystrom <fredriknystroms@gmail.com>
2024-02-26 20:47:10 +0100

    test 4.10

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,20 +1,28 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const { oneBlog, multipleBlogs } = require('./test_helper')
-const blog = require('../models/blog')
+const { newBlog, multipleBlogs } = require('./test_helper')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-test.only('returns all notes', async () => {
-    const n_expected_notes = 3
-    const response = await api.get('/api/blogs')
-    assert(response.body.length, n_expected_notes)
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = multipleBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
 })
 
-test.only('blog post unique identifier is named id', async () => {
+test('returns all notes', async () => {
+    const response = await api.get('/api/blogs')
+    assert(response.body.length, multipleBlogs.length)
+})
+
+test('blog post unique identifier is named id', async () => {
   const response = await api.get('/api/blogs')
   if (response.body.length > 0) {
     const blogPost = response.body[0];
@@ -22,7 +30,18 @@ test.only('blog post unique identifier is named id', async () => {
   } else {
     throw new Error('No blog posts found');
   }
-})  
+})
+
+test.only('posting a new blog', async () => {
+  console.log("here")
+  await api.post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, multipleBlogs.length+1)
+}) 
 
 after(async () => {
   await mongoose.connection.close()
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,15 +1,11 @@
 const Blog = require('../models/blog')
 
-const oneBlog = [
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-    likes: 5,
-    __v: 0
-  }
-]
+const newBlog = {
+  title: "newest post!",
+  author: "Fredrik",
+  url: "hello.com",
+  likes: 12
+}
 
 const multipleBlogs = [
     {
@@ -29,11 +25,19 @@ const multipleBlogs = [
         __v: 0
       },
       {
-        _id: 'b2422aa71b54a676234d17f8',
+        _id: '5a422aa71b543276234d47f8',
         title: 'Go To Statement Considered Harmful',
         author: 'Edsger W. Dijkstra',
         url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
         likes: 40,
+        __v: 0
+      },
+      {
+        _id: '5a562aa71b543276234d47f8',
+        title: 'Post 4',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 90,
         __v: 0
       },
   ]
@@ -53,5 +57,5 @@ const notesInDb = async () => {
 */
 
 module.exports = {
-  oneBlog, multipleBlogs
+  newBlog, multipleBlogs
 }
